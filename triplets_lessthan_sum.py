a7a2c7f96d044d365745c0fc1673b8433de41921
murraycoding <67034749+murraycoding@users.noreply.github.com>
2020-08-22 23:28:33 -0400

    Add files via upload

---
+++ triplets_lessthan_sum.py
@@ -0,0 +1,8 @@
+def triplets_lessthan_sum(sum, vals):
+    print(f'Type of sum = {type(sum)}')
+    print(f'Type of vals = {type(vals)}')
+    if type(vals) == list:
+        print('It worked!!')
+    return
+
+triplets_lessthan_sum(4, [1,2,3,4])
